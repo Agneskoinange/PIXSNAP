4c2643a6d8bde4ca5d03dd348c2e431b3cab6580
Agnes <koinageagness@gmail.com>
2022-05-30 11:00:58 +0300

    Create Models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,85 @@
 from django.db import models
 
 # Create your models here.
+
+# Location model to enable us choose location
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.save()
+
+
+# iterable tuple to use as choices of category field
+
+
+ORDER_CHOICES = (
+    ('11', 'Vegetation'),
+    ('12', 'Trees'),
+    ('13', 'Nature'),
+    ('14', 'Abstract'),
+    ('15', 'IT & Computers'),
+    ('16', 'Animals'),
+    ('17', 'Illustrations'),
+    ('18', 'Arts'),
+    ('19', 'Business'),
+    ('20', 'Industries'),
+    ('21', 'Objects'),
+    ('22', 'Technology'),
+    ('23', 'People'),
+    ('24', 'Travel'),
+)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    category_choices = models.CharField(max_length=10, choices=ORDER_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_catgory(self):
+        self.save()
+
+
+class Image(models.Model):
+    image_name = models.CharField(max_length=20)
+    image_description = models.CharField(max_length=30)
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    image = models.ImageField(upload_to='photos/', null="True", blank="True")
+
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def search_by_category(cls, search_term):
+        display = cls.objects.filter(category__name__icontains=search_term)
+        return display
+
+  
+    @classmethod
+    def filter_by_location(cls):
+        display = cls.objects.filter(
+            location__name__icontains='Silicon Valley')
+        return display
+
+    class Meta:
+        ordering = ['image_name']
