b5fd2428dd6428eed9b3a42227770a2fe634cef3
Agnes <koinageagness@gmail.com>
2022-05-30 11:03:28 +0300

    Create views handler for images

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Image
 # Create your views here.
+
+
+def images(request):
+    '''
+    function to display the index page
+    '''
+    images = Image.objects.all()
+    return render(request, 'images.html', {"images": images})
+
