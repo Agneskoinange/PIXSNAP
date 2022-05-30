421a8b3fe5ab89ecbfc7b9b9784c22a8efc27499
Agnes <koinageagness@gmail.com>
2022-05-30 11:59:33 +0300

    modify urls to re_path

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
 from . import views
+from django.urls import re_path
 
 urlpatterns=[
-    url(r'^$', views.images, name='images'),
-    url(r'^home/$', views.home, name='home'),
-    url(r'^search/$', views.search_results, name='search_results'),
-    url(r'^image/(\d+)', views.image, name='image'),
-    url(r'^Silicon_Valley/', views.Silicon_Valley, name='Silicon_Valley'),
+    re_path(r'^$', views.images, name='images'),
+    re_path(r'^home/$', views.home, name='home'),
+    re_path(r'^search/$', views.search_results, name='search_results'),
+    re_path(r'^image/(\d+)', views.image, name='image'),
+    re_path(r'^Silicon_Valley/', views.Silicon_Valley, name='Silicon_Valley'),
 ]
