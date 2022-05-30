d837b367ff1b80cc1e4fe2c638dc9b45ac621f56
Agnes <koinageagness@gmail.com>
2022-05-30 11:35:18 +0300

    add url routs for vies handlers

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns=[
     url(r'^$', views.images, name='images'),
+    url(r'^home/$', views.home, name='home'),
+    url(r'^search/$', views.search_results, name='search_results'),
+    url(r'^image/(\d+)', views.image, name='image'),
+    url(r'^Silicon_Valley/', views.Silicon_Valley, name='Silicon_Valley'),
 ]
