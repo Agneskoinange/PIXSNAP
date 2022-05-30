f08054359926bf09e557f4b2bc46a91900552460
Agnes <koinageagness@gmail.com>
2022-05-30 11:34:20 +0300

    add views function for image home and html

--- app/views.py
+++ app/views.py
@@ -18,3 +18,35 @@ def Silicon_Valley(request):
 
     images = Image.filter_by_location()
     return render(request, 'Silicon Valley.html', {"imagey": images},)
+
+def image(request, image_id):
+
+    images = Image.objects.get(id=image_id)
+
+    return render(request, "image.html", {"images": images})
+
+
+def home(request):
+    '''
+    home function to display home images
+    '''
+
+    return render(request, 'home.html')
+
+
+def search_results(request):
+    '''
+    search function to display search search_results
+    args:
+    order defines category
+    '''
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html', {"message": message, "images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message": message})
