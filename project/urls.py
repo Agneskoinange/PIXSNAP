0f7317e6baae4e023f61c8ed28fd4d87508988cd
Agnes <koinageagness@gmail.com>
2022-05-31 17:23:45 +0300

    add scripts

--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.urls import re_path
 
+
 from django.conf import settings
 from django.conf.urls.static import static
 
