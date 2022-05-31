306e15888c7ae8140e8add3b6046f01d9a1b3eb8
Agnes <koinageagness@gmail.com>
2022-05-31 08:08:44 +0300

    register media root

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from . import views
 from django.urls import re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     re_path(r'^$', views.images, name='images'),
@@ -8,3 +10,6 @@ urlpatterns=[
     re_path(r'^image/(\d+)', views.image, name='image'),
     re_path(r'^Silicon_Valley/', views.Silicon_Valley, name='Silicon_Valley'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
