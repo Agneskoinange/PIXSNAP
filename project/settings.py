9136ec0278b073ba69f7b813cc5732f7dac61df7
Agnes <koinageagness@gmail.com>
2022-06-01 12:49:46 +0300

    update config files

--- project/settings.py
+++ project/settings.py
@@ -34,6 +34,14 @@ else:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+if ON_HEROKU:
+    DATABASE_URL = 'postgresql://<postgresql>'
+else:
+    DATABASE_URL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+
+
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 "import django_heroku" 
