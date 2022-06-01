6f2e64820a9ed86a6b7741d13b6411980f9c8a3d
Agnes <koinageagness@gmail.com>
2022-06-01 13:43:12 +0300

    change setting engine  engine configurations

--- project/settings.py
+++ project/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pixsnap',
         'USER': 'nessie',
         'PASSWORD': 'agnes1234',
